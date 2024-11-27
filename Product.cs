1a56bec02f2ad7db2d2860b51559a142be2185f1
Lama1510 <115421526+Lama1510@users.noreply.github.com>
2024-11-27 21:06:51 +0200

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product_class
+{
+    internal class Product
+    {
+
+        public int Id;
+        public string Name;
+        public string Description;
+        public double Price;
+
+        public void print()
+        {
+            Console.WriteLine($" Product id is {Id}  , name is {Name} , description is {Description} and its price is {Price}");
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace product_class
+{
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Product productOne = new Product();
+            Product productTwo= new Product();
+            Product productThree = new Product();
+
+
+
+
+            productOne.Id = 1;
+            productTwo.Id = 2;
+            productThree.Id = 3;
+
+            productOne.Name = "Apple";
+            productTwo.Name = " Orange";
+            productThree.Name = "banana";
+
+
+            productOne.Description = "Description 1";
+            productTwo.Description = "Description 2 ";
+            productThree.Description = "Description 3 ";
+
+
+
+            productOne.Price = 10.5;
+            productTwo.Price = 8;
+            productThree.Price = 5.6;
+
+
+            productOne.print();
+            productTwo.print();
+            productThree.print();
+
+        }
+    }
+}
